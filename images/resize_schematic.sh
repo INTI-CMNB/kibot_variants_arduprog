cf435630e7e0ea3ad74ca84b569f64da0e434b7d
Salvador E. Tropea <salvador@inti.gob.ar>
2023-04-02 10:26:27 -0300

    More coherence between schematic and PCB
    - Same time format, using the generation time stamp
    - Same title, adding the variant
    - Same worksheet. Why KiCad 6 doesn't respect the first page in the PCB?
    - Same revision, short git hash

---
+++ images/resize_schematic.sh
@@ -0,0 +1,3 @@
+convert t1-schematic.pdf -trim -resize 480x t1-schematic.png
+convert t1-schematic_USB.pdf -trim -resize 480x t1-schematic_USB.png
+convert t1-schematic_XTAL.pdf -trim -resize 480x t1-schematic_XTAL.png
